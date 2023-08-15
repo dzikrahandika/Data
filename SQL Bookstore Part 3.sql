44749f9b0bef813689f57664aeaba1ecef1f1fb7
Dzikra Handika <110003756+dzikrahandika@users.noreply.github.com>
2023-08-15 15:02:23 +0700

    Add files via upload

---
+++ SQL Bookstore Part 3.sql
@@ -0,0 +1,14 @@
+/* #List the titles of books that were published before the year 1950, along with their authors' names and publication years.
+SELECT title, author_name, publication_year FROM Book
+JOIN Authors ON Book.author_id = Authors.author_id
+WHERE publication_year < 1950;
+*/
+
+# Find the top 3 genres that have the highest average book prices. Include the genre names and their corresponding average prices.
+
+SELECT genre_name, ROUND(AVG(price), 2) AS AveragePrices
+FROM Genres
+JOIN Book ON Genres.genre_id = Book.genre_id
+GROUP BY genre_name
+ORDER BY AveragePrices DESC
+LIMIT 3;
